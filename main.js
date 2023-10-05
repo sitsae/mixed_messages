40be599db0d3848d3b5c0ee726d1d26949239ee4
Sigurd Torve Sæther <torve_s@live.no>
2023-10-05 14:29:11 +0200

    shorter code

--- main.js
+++ main.js
@@ -1,37 +1,33 @@
+// 50 ord av AVSO
 const verbal = ["Smilende", "Grønn", "Høylytt", "Sulten", "Trist", "Glad", "Trett", "Ivrig", "Forsiktig", "Varm", "Nysgjerrig", "Engstelig", "Gledesstrålende", "Entusiastisk", "Travelt", "Overrasket", "Høyt", "Selvsikker", "Hjelpsom", "Fascinert", "Utslitt", "Irritert", "Romantisk", "Skremt", "Takknemlig", "Entusiastisk", "Forvirret", "Eventyrlysten", "Engasjert", "Høyt", "Sinte", "Eksentrisk", "Skremmende", "Entusiastisk", "Lykkelig", "Overrasket", "Kreativ", "Utmattet", "Forbauset", "Fornøyd", "Overrasket", "Hengiven", "Entusiastisk", "Trist", "Uimponert", "Vennlig", "Forundret", "Bekymret", "Euforisk", "Overveldet"];
 const verb = ["danset", "vokste", "lo", "spiste", "gråt", "sang", "sovnet", "studerte", "malede", "omfavnet", "utforsket", "ventet", "hilste", "delte", "jobbet", "åpnet", "skrek", "presenterte", "reddet", "observerte", "løp", "kritiserte", "fridde", "hoppet", "donerte", "lærte", "søkte", "utforsket", "hjalp", "applauderte", "diskuterte", "oppfant", "forfulgte", "smakte", "forelsket", "oppdaget", "skapte", "fullførte", "vant", "malte", "besøkte", "pleide", "solgte", "begro", "kritiserte", "hjalp", "oppdaget", "trøstet", "feiret", "takket"];
 const subjekt = ["barnet", "planten", "publikum", "katten", "jenta", "fuglene", "mannen", "studentene", "kunstneren", "kjæresten", "barna", "foreldrene", "vennene", "frivillige", "teamet", "boksen", "fotballspilleren", "lederen", "livredderen", "forskeren", "maratonløperen", "sjefen", "kjæresten", "katten", "samfunnet", "læreren", "journalisten", "oppdagelsesreisende", "hjemløse", "publikum", "politikerne", "oppfinneren", "helten", "matkritikeren", "paret", "forskeren", "kunstneren", "maratonløperen", "lotterivinneren", "barnet", "turisten", "sykepleieren", "gründeren", "familien", "anmelderen", "naboen", "astronauten", "venninnen", "vinnerne", "studenten", "professoren"];
 const objekt = ["under stjernene", "i vinduskarmen", "under forestillingen", "sin favorittmat", "etter bruddet", "i morgenlyset", "på sofaen", "for eksamen", "sitt mesterverk", "i regnet", "skogen", "på nyhetene", "med omfavnelser", "mat til de trengende", "på prosjektet", "med en gave", "etter scoringen", "strategiplanen", "den druknende", "solformørkelsen", "over målstreken", "den dårlige beslutningen", "på stranda", "fra høyden", "til veldedighet", "elevene", "sannheten", "den ukjente øya", "hjemløse", "etter konserten", "den kontroversielle loven", "en ny gadget", "helten", "gourmetretten", "i solnedgangen", "en sjelden art", "et abstrakt maleri", "løpet", "millioner", "sitt første bilde", "det historiske slottet", "pasienten", "sitt første produkt", "den nedstemte", "seieren", "professoren"];
 
-const konstaterendeSetning = () => {
-    const sub = subjekt[Math.floor(Math.random()* subjekt.length)];
-    const ver = verb[Math.floor(Math.random()* verb.length)];
-    const obj = objekt[Math.floor(Math.random()* objekt.length)];
+const vrbl = verbal[Math.floor(Math.random()* verbal.length)];
+const sub = subjekt[Math.floor(Math.random()* subjekt.length)];
+const ver = verb[Math.floor(Math.random()* verb.length)];
+const obj = objekt[Math.floor(Math.random()* objekt.length)];
 
-    return sub + ' ' + ver + ' ' + obj;
-};
+// funksjon for produksjon av SVO-setning
+const konstaterendeSetning = sub + ' ' + ver + ' ' + obj;
 
-const sporresetning = () => {
-    const sub = subjekt[Math.floor(Math.random()* subjekt.length)];
-    const ver = verb[Math.floor(Math.random()* verb.length)];
-    const obj = objekt[Math.floor(Math.random()* objekt.length)];
+// funksjon for produksjon av VSO-setning
+const sporresetning = ver + ' ' + sub + ' ' + obj + '?';
 
-    return ver + ' ' + sub + ' ' + obj + '?';
-};
+// funksjon for produksjon av AVSO-setning
+const utsagnssetning = vrbl + ' ' + ver + ' ' + sub + ' ' + obj;
 
-const utsagnssetning = () => {
-    const vrbl = verbal[Math.floor(Math.random()* verbal.length)];
-    const sub = subjekt[Math.floor(Math.random()* subjekt.length)];
-    const ver = verb[Math.floor(Math.random()* verb.length)];
-    const obj = objekt[Math.floor(Math.random()* objekt.length)];
+setningsArr = [konstaterendeSetning, sporresetning, utsagnssetning];
 
-
-    return vrbl + ' ' + ver + ' ' + sub + ' ' + obj;
-};
+const velgSetningstype = () => {
+    randI = Math.floor(Math.random() * setningsArr.length)
+    return setningsArr[randI]
+}
 
 
+// console.log(konstaterendeSetning);
+// console.log(sporresetning);
+// console.log(utsagnssetning);
 
-
-console.log(konstaterendeSetning());
-console.log(sporresetning());
-console.log(utsagnssetning());
+console.log(velgSetningstype());
